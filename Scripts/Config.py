33482f9712ee67c04d5610884fa33fe876d572ac
Michael Dwyer <mdwyer84+github@gmail.com>
2022-08-13 13:12:42 -0400

    Adding placeholder user stats

--- Scripts/Config.py
+++
@@ -1,4 +0,0 @@
-file_path = 'FULL_FILE_PATH'
-debug_file_path = 'Debug Chat.json'
-
-google_messages_encoding = 'utf8'
--- Scripts/GenericChatUtil.py
+++ Scripts/GenericChatUtil.py
@@ -8,7 +8,21 @@ class GenericConversation:
 
 class GenericUserStats:
     user_id = ""
-    message_count = ""
+    message_count = 0
+    question_count = 0    
+    response_count = 0
+    total_char_count = 0
+    longest_message = 0
+    image_count = 0
+    longest_chain = 0
+    longest_delay_caused = 0
+    am_pm_ratio = 0
+    emoji_count = 0
+    url_count = 0
+    exclamation_mark_count = 0
+    reaction_count = 0
+    most_obscure_word_used = ""
+    longest_sentence = 0
     
 class GenericConversationStats:
     start_timestamp = 0
